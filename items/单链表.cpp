a4d61590b83e7b3644625aeb439854bd03379415
zzxxxrrr <15991474633@163.com>
2026-03-03 23:18:00 +0800

    单链表插入和删除

--- items/单链表.cpp
+++ items/单链表.cpp
@@ -26,6 +26,62 @@ void CreateList(LinkList &L,int arr[],int n){//利用数组创建一个链表
      rear->next=NULL;
 }
 
+bool ListInsert(LinkList &L,int i,int e){//插入元素
+    if(i<1) return false;
+    LNode *p;//表示当前节点
+    int j=0;//表示当前是第几个节点
+    p = L;//指向头节点
+    if(p!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL) return false;
+    LNode *s=(LNode*)malloc(sizeof(LNode));
+    s->next=p->next;
+    s->data=e;
+    p->next=s;
+    return true;
+}
+
+bool InsertNextNode(LNode *p,int e){//后插操作,在节点p后插入元素
+    if(p==NULL) return false;
+    LNode *s=(LNode*)malloc(sizeof(LNode));
+    if(s==NULL) return false;
+    s->data=e;
+    s->next=p->next;
+    p->next=s;
+    return true;
+
+}
+
+bool InsertPriorNode(LNode *p,int e){//直接交换,时间复杂度为1
+    if(p==NULL) return false;
+    LNode *s=(LNode*)malloc(sizeof(LNode));
+    s->next=p->next;
+    p->next=s;
+    s->data=p->data;
+    p->data=e;
+    return true;
+}
+
+bool Delete(LinkList &L,int i,int &e){//删除第i个位置的元素
+    if(i<1) return false;
+    LNode *p;
+    int j=0;
+    p=L;
+    while(p!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(p==NULL) return false;
+    if(p->next==NULL) return false;
+    LNode *q=p->next;
+    p->next=q->next;
+    int e=q->data;
+    free(q);
+    return false;
+}
+
 int main(){
     LinkList L=NULL;
     int data[] = {10, 20, 30, 40};
